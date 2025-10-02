c13baeb623a50c2402bc5570ae0904f2da9cd84b
Akasaka Ryuunosuke <local@local.local>
2025-10-02 18:30:51 +0900

    this at least spins the rainbow stable

--- main.c
+++ main.c
@@ -9,21 +9,32 @@
 #define STRIP_PER_WEDGE  5
 #define WEDGE_PER_MACHINE 12
 #define WORDS_IN_FRAMEBUFFER (LED_PER_STRIP * STRIP_PER_WEDGE * WEDGE_PER_MACHINE)
-#define LED_FREQ 1200000
-#define DWELL_TIME 128
+#define LED_FREQ 2000000
+// The threshold of L time to determine we are definitely at start of a new frame
+#define DWELL_TIME 1024
 #define LED_PIN 1
+
+uint _pio_offset = 0;
 
 void ws2812_pio_init(PIO pio, uint sm, uint freq, uint pin) {
     pio_gpio_init(pio, pin);
+    pio_gpio_init(pio, pin + 1);
+    pio_gpio_init(pio, pin + 2);
     pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, false);
+    pio_sm_set_consecutive_pindirs(pio, sm, pin + 1, 2, true);
 
     uint offset = pio_add_program(pio, &ws2812in_program);
+    _pio_offset = offset;
 
     pio_sm_config c = pio_get_default_sm_config();
     sm_config_set_in_pin_base(&c, pin);
     sm_config_set_in_pin_count(&c, 1);
     sm_config_set_jmp_pin(&c, pin);
+    sm_config_set_sideset_pin_base(&c, pin + 1);
+    sm_config_set_sideset(&c, 2, true, false);
+    sm_config_set_wrap(&c, offset + ws2812in_wrap_target, offset + ws2812in_wrap);
     sm_config_set_in_shift(&c, false, true, 24);
+    sm_config_set_out_shift(&c, false, false, 32);
     sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_NONE);
 
     unsigned int cycles_per_bit = T1 + T2 + T3;
@@ -42,7 +53,7 @@ void ws2812_pio_capture_frame(PIO pio, uint sm, uint dma_channel, uint32_t* dst,
     channel_config_set_read_increment(&d, false);
     channel_config_set_write_increment(&d, true);
     channel_config_set_dreq(&d, pio_get_dreq(pio, sm, false));
-    // channel_config_set_high_priority(&d, true);
+    channel_config_set_high_priority(&d, true);
 
     dma_channel_configure(
         dma_channel,
@@ -54,6 +65,7 @@ void ws2812_pio_capture_frame(PIO pio, uint sm, uint dma_channel, uint32_t* dst,
     );
 
     pio_sm_put(pio, sm, DWELL_TIME);
+    pio_sm_exec(pio, sm, pio_encode_jmp(_pio_offset));
     pio_sm_set_enabled(pio, sm, true);
 }
 
@@ -83,9 +95,8 @@ int main()
     static uint32_t framebuffer[WORDS_IN_FRAMEBUFFER] = { 0 };
     static uint32_t framebuffer2[WORDS_IN_FRAMEBUFFER] = { 0 };
     bool in_side = false;
-       
+
     while (true) {
-     while(!stdio_usb_connected()) ;
         ws2812_pio_capture_frame(pio, sm, dma_channel, in_side ? framebuffer2 : framebuffer, WORDS_IN_FRAMEBUFFER);
         transfer_frame(in_side ? framebuffer : framebuffer2, WORDS_IN_FRAMEBUFFER);
         dma_channel_wait_for_finish_blocking(dma_channel);
